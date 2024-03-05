af5fb4fd52cfc0dfa373b7c7688f274eea242a9e
htk-opash <htk.opash@gmail.com>
2024-03-05 18:31:40 +0530

    Secure sockets, routes and Sensitive data

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,13 @@
 "use strict";
 
 const mysql = require("mysql2");
+const environment = require("../src/environments/environment");
 
 const db = mysql.createConnection({
-  host: "37.27.15.8",
-  user: "root",
-  password: "JYq1Nyr2l7Cf",
-  database: "wheels-tube",
-  // host: '135.181.104.107',
-  // user: 'root',
-  // password: 'JYq1Nyr2l7Cf',
-  // database: 'freedom-buzz'
+  host: environment.DB_HOST,
+  user: environment.DB_USER,
+  password: environment.DB_PASS,
+  database: environment.DB_NAME
 });
 
 db.connect(function (err) {
--- src/routes/unsubscribeProfile.routes.js
+++ src/routes/unsubscribeProfile.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const unsubscribeProfileController = require("../controllers/unsubscribeProfile.controller");
 
+const authorize = require("../middleware/authorize");
+router.use(authorize.authorization);
 router.post("/create", unsubscribeProfileController.create);
 router.delete("/remove/:id", unsubscribeProfileController.remove);
 router.get("/getByProfileId/:profileId", unsubscribeProfileController.getByProfileId);
